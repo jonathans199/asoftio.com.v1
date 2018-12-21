66963ddf3e2fe83ebdfb9f72c439b40e4cd3aab4
jonathan <jso1919@gmail.com>
2018-12-21 12:07:01 -0500

    section team

--- vue.config.js
+++ vue.config.js
@@ -1,11 +1,7 @@
-module.exports = {
-  configureWebpack: {
-    // other webpack options to merge in ...
-  },
-  // devServer Options don't belong into `configureWebpack`
-  // devServer: {
-  //   port: 3000,
-  //   host: 'localhost'
-  // },
-};
+  module.exports = {
+    chainWebpack: config => {
+        config.module.rules.delete('eslint');
+    }
+ }
+ 
 
